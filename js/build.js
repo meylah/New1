cb070b4abfbef3adc961898140c85bbd48aa5149
Hammid1987 <hh9af@virginia.edu>
2017-03-07 02:30:33 -0500

    Update build.js

--- js/build.js
+++ js/build.js
@@ -37,7 +37,67 @@ emap.setView({
       console.log('data not found');
     })
   }
-});	
+});
+		// ///////////////Sensor Reading
+// get html elements to append with event values
+var orientationEl = document.getElementById('orientation'),
+  accelerationEl = document.getElementById('acceleration'),
+  accIncGravEl = document.getElementById('accincgrav'),
+  rotationEl = document.getElementById('rotation'),
+  intervalEl = document.getElementById('interval');
+ 
+if (window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false)
+}
+
+function deviceOrientationHandler(evt) {
+  var orientationData = evt;
+  // compass direction
+  orientationEl.children[1].innerHTML = evt.alpha;
+
+  // vertical tilt
+  orientationEl.children[3].innerHTML = evt.beta;
+
+  // horizontal tilt
+  orientationEl.children[5].innerHTML = evt.gamma;
+}
+
+if (window.DeviceMotionEvent) {
+  window.addEventListener('devicemotion', deviceMotionHandler, false);
+}
+
+function deviceMotionHandler(evt) {
+  var motionData = evt;
+  // acceleration
+  var evAcceleration = evt.acceleration;
+
+  accelerationEl.children[1].innerHTML = evAcceleration.x;
+  accelerationEl.children[3].innerHTML = evAcceleration.y;
+  accelerationEl.children[5].innerHTML = evAcceleration.z;
+
+  // acceleration including gravity
+  var accIncGravity = evt.accelerationIncludingGravity;
+
+  accIncGravEl.children[1].innerHTML = accIncGravity.x;
+  accIncGravEl.children[3].innerHTML = accIncGravity.y;
+  accIncGravEl.children[5].innerHTML = accIncGravity.z;
+
+  // rotation rate
+  var rotationRate = evt.rotationRate;
+
+  rotationEl.children[1].innerHTML = rotationRate.alpha;
+  rotationEl.children[3].innerHTML = rotationRate.beta;
+  rotationEl.children[5].innerHTML = rotationRate.gamma;
+  emap.applyDeltaScaleRotation({
+    rotation:rotationRate.gamma,
+    callback: function(err, state) {
+      handleMapState(err,state,true);
+    }
+  });
+  // interval
+  var interval = evt.interval;
+  intervalEl.children[1].innerHTML = interval;
+}
 /*---- stop dragging of tiles ----*/
 //  domDelegate = require('dom-delegate'),
 // as images are loaded dynamicaly delegate event on class .tile
@@ -177,66 +237,7 @@ mc.on('rotatemove', function (ev) {
     });
     lastRot = ev.rotation;
 });
-	// ///////////////Sensor Reading
-// get html elements to append with event values
-var orientationEl = document.getElementById('orientation'),
-  accelerationEl = document.getElementById('acceleration'),
-  accIncGravEl = document.getElementById('accincgrav'),
-  rotationEl = document.getElementById('rotation'),
-  intervalEl = document.getElementById('interval');
- 
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', deviceOrientationHandler, false)
-}
 
-function deviceOrientationHandler(evt) {
-  var orientationData = evt;
-  // compass direction
-  orientationEl.children[1].innerHTML = evt.alpha;
-
-  // vertical tilt
-  orientationEl.children[3].innerHTML = evt.beta;
-
-  // horizontal tilt
-  orientationEl.children[5].innerHTML = evt.gamma;
-}
-
-if (window.DeviceMotionEvent) {
-  window.addEventListener('devicemotion', deviceMotionHandler, false);
-}
-
-function deviceMotionHandler(evt) {
-  var motionData = evt;
-  // acceleration
-  var evAcceleration = evt.acceleration;
-
-  accelerationEl.children[1].innerHTML = 10;
-  accelerationEl.children[3].innerHTML = evAcceleration.y;
-  accelerationEl.children[5].innerHTML = evAcceleration.z;
-
-  // acceleration including gravity
-  var accIncGravity = evt.accelerationIncludingGravity;
-
-  accIncGravEl.children[1].innerHTML = accIncGravity.x;
-  accIncGravEl.children[3].innerHTML = accIncGravity.y;
-  accIncGravEl.children[5].innerHTML = accIncGravity.z;
-
-  // rotation rate
-  var rotationRate = evt.rotationRate;
-
-  rotationEl.children[1].innerHTML = rotationRate.alpha;
-  rotationEl.children[3].innerHTML = rotationRate.beta;
-  rotationEl.children[5].innerHTML = rotationRate.gamma;
-  emap.applyDeltaScaleRotation({
-    rotation:rotationRate.gamma,
-    callback: function(err, state) {
-      handleMapState(err,state,true);
-    }
-  });
-  // interval
-  var interval = evt.interval;
-  intervalEl.children[1].innerHTML = interval;
-}
 /*mc.on('rotateend', function(ev) {
   console.log('rotateend');
   emap.resetTileMatrix({
