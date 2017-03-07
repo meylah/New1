64b8fd89ea12c3642ccd6d387a84fb377b57e3ac
Hammid1987 <hh9af@virginia.edu>
2017-03-06 21:37:11 -0500

    Update build.js

--- js/build.js
+++ js/build.js
@@ -69,36 +69,36 @@ emap.setView({
 //     }
 //   });
 // }
-if (window.DeviceOrientationEvent) {
-  document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+//if (window.DeviceOrientationEvent) {
+  //document.getElementById("doEvent").innerHTML = "DeviceOrientation";
   // Listen for the deviceorientation event and handle the raw data
-  window.addEventListener('deviceorientation', function(eventData) {
+  //window.addEventListener('deviceorientation', function(eventData) {
     // gamma is the left-to-right tilt in degrees, where right is positive
-    var tiltLR = eventData.gamma;
+   // var tiltLR = eventData.gamma;
 
     // beta is the front-to-back tilt in degrees, where front is positive
-    var tiltFB = eventData.beta;
+   // var tiltFB = eventData.beta;
 
     // alpha is the compass direction the device is facing in degrees
-    var dir = eventData.alpha
+ //   var dir = eventData.alpha
 
     // call our orientation event handler
-    deviceOrientationHandler(tiltLR, tiltFB, dir);
-  }, false);
-} else {
-  document.getElementById("doEvent").innerHTML = "Not supported."
-}
-document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-document.getElementById("doDirection").innerHTML = Math.round(dir);
+  //  deviceOrientationHandler(tiltLR, tiltFB, dir);
+ // }, false);
+//} else {
+//  document.getElementById("doEvent").innerHTML = "Not supported."
+//}
+//document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+//document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+//document.getElementById("doDirection").innerHTML = Math.round(dir);
 
 // Apply the transform to the image
-var logo = document.getElementById("imgLogo");
-logo.style.webkitTransform =
-  "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
-logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
-logo.style.transform =
-  "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+//var logo = document.getElementById("imgLogo");
+//logo.style.webkitTransform =
+//  "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+//logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
+//logo.style.transform =
+//  "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
 /*---- stop dragging of tiles ----*/
 //  domDelegate = require('dom-delegate'),
 // as images are loaded dynamicaly delegate event on class .tile
