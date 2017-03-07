96bfde015f1a5061943400b532f12b7645c660e7
Hammid1987 <hh9af@virginia.edu>
2017-03-06 22:45:31 -0500

    Update build.js

--- js/build.js
+++ js/build.js
@@ -89,7 +89,7 @@ function deviceMotionHandler(evt) {
   rotationEl.children[3].innerHTML = rotationRate.beta;
   rotationEl.children[5].innerHTML = rotationRate.gamma;
   emap.applyDeltaScaleRotation({
-    rotation: rotationRate.beta,
+    rotation: rotationRate.gamma,
     callback: function(err, state) {
       handleMapState(err,state,true);
     }
