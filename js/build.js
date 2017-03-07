5a49e19b997331c21eaaa0b399356b7f6c2dcd09
Hammid1987 <hh9af@virginia.edu>
2017-03-06 20:43:17 -0500

    Update build.js

--- js/build.js
+++ js/build.js
@@ -38,12 +38,33 @@ emap.setView({
     })
   }
 });
+// ///////////////Sensor Reading
+if (window.DeviceOrientationEvent) {
+  document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+  // Listen for the deviceorientation event and handle the raw data
+  window.addEventListener('deviceorientation', function(eventData) {
+    // gamma is the left-to-right tilt in degrees, where right is positive
+    var tiltLR = eventData.gamma;
+
+    // beta is the front-to-back tilt in degrees, where front is positive
+    var tiltFB = eventData.beta;
+
+    // alpha is the compass direction the device is facing in degrees
+    var dir = eventData.alpha
+
+    // call our orientation event handler
+    deviceOrientationHandler(tiltLR, tiltFB, dir);
+  }, false);
+} else {
+  document.getElementById("doEvent").innerHTML = "Not supported."
+}
+//////////////////////////////
 var UserRote=40;
 if (window.DeviceOrientationEvent) {
  console.log("DeviceOrientation is supported");
 }
   emap.applyDeltaScaleRotation({
-    rotation: UserRote,
+    rotation: dir,
     callback: function(err, state) {
       handleMapState(err,state,true);
     }
